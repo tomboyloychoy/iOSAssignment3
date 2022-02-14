fe092afaeb374e568c7a16624f5da0130e63cfae
tomboyloychoy <89024064+tomboyloychoy@users.noreply.github.com>
2022-02-13 22:58:17 -0500

    Add files via upload

---
+++ HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import AVKit
+
+struct ExerciseView: View {
+  let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+  let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+  let index: Int
+  let interval: TimeInterval = 30
+
+  var body: some View {
+    GeometryReader { geometry in
+      VStack {
+        HeaderView(titleText: exerciseNames[index])
+          .padding(.bottom)
+        if let url = Bundle.main.url(
+          forResource: videoNames[index],
+          withExtension: "mp4") {
+          VideoPlayer(player: AVPlayer(url: url))
+            .frame(height: geometry.size.height * 0.45)
+        } else {
+          Text("Couldn't find \(videoNames[index]).mp4")
+            .foregroundColor(.red)
+        }
+        Text(Date().addingTimeInterval(interval), style: .timer)
+          .font(.system(size: 90))
+        Button("Start/Done") { }
+          .font(.title3)
+          .padding()
+        RatingView()
+          .padding()
+        Spacer()
+        Button("History") { }
+          .padding(.bottom)
+      }
+    }
+  }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+  static var previews: some View {
+    ExerciseView(index: 0)
+  }
+}
